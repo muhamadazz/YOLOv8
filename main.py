ea6b32911cc3341d6167482e45bbcc53b017f530
muhamadazz <153073061+muhamadazz@users.noreply.github.com>
2024-06-23 12:55:52 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLO
+import cv2
+import time
+
+model = YOLO("best1.pt")
+
+cam = cv2.VideoCapture(0)
+if not cam.isOpened():
+    raise("no camera")
+
+while True :
+    ret, image = cam.read()
+    if not ret:
+        break
+    
+    _time_mulai = time.time()
+    result = model.predict(image, show = True)
+
+    print("waktu", time.time()-_time_mulai)
+
+    _key = cv2.waitKey(1)
+    if _key == ord('q'):
+        break
+
+
+cam.release()
+cv2.destroyAllWindows()
